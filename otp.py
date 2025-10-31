ec5977b1c7a5a54e4c07bf6db4c1cd632d533f8d
Saif Chhipa <126426547+Cyber-Saif@users.noreply.github.com>
2025-10-31 15:32:45 -0700

    Refactor OTP implementation and improve file handling

--- otp.py
+++ otp.py
@@ -1,5 +1,8 @@
-import random, pathlib, argparse, base64
-letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import random
+import pathlib
+import argparse
+import base64
+from os import urandom
 
 # Reading the input file
 def file_reader(file_location):
@@ -20,10 +23,10 @@ def file_writter(key, ciphertext, recoverdtext):
     if ciphertext:
         #Writing the Log
         data = f"KEY: {key}\nCIPHERTEXT: {str(ciphertext)[2:-1]}"
-        with open("ciphertext_log.txt", "+w") as output:
+        with open("otp_log.txt", "w+") as output:
             output.write(data)
         #Writing the encrypted data only
-        with open("cipher.txt", "w+") as cipher:
+        with open("ciphertext.txt", "w+") as cipher:
             cipher.write(str(ciphertext)[2:-1])
     
     elif recoverdtext:
@@ -33,14 +36,12 @@ def file_writter(key, ciphertext, recoverdtext):
         print(f"[+] Decrypted message stored in the 'decrypted.txt'\n")
     
     else:
-        print("[-] Something wrong happended while writing...check file permissions..")
+        print("[-] Something wrong happended while writing...Check file permissions!!!")
 
 #Random Key Generator
 def key_generator(message):
-    key = ""
-    while (len(key)) != (len(message)):    
-        value = random.choice(letters)
-        key+=value
+    key = urandom(len(message))
+
     return key
 
 # XOR Function
@@ -50,14 +51,11 @@ def xor_fuction(key, message):
     for i in range(len(key)):
         #Character to Decimal
         ascii_key = ord(key[i])
-        ascii_letter = ord(message[i])
-        
-        #XOR
+        ascii_letter = ord(message[i])        
+        #XORing the values
         xor = ascii_key ^ ascii_letter
         #For troubleshooting only
-        #print(f"{key[i]} [{ascii_key}] XOR {message[i]} [{ascii_letter}] = {chr(xor)} [{xor}]")
-        
-        
+        #print(f"{key[i]} [{ascii_key}] XOR {message[i]} [{ascii_letter}] = {chr(xor)} [{xor}]")        
         #Decimal to Char
         xored_message+=chr(xor)
 
@@ -67,36 +65,45 @@ def xor_fuction(key, message):
 def one_time_pad(user_key, mode, input_file):
     #This function is for either encrypting or decrypting the message
     #based on the user's choice
-    
-    #For encryption, we will generate a random key
-    if mode == encrypt_option:
-        message = file_reader(input_file)
-        #Generating a random key
-        key = key_generator(message)
-        ciphertext = xor_fuction(key, message)
-        #Encoding
-        byte_ciphertext = ciphertext.encode("utf-8")
-        ciphertext = base64.b64encode(byte_ciphertext)
+    return_key = None
+    return_message = None
+    try:
+        #For encryption, we will generate a random key
+        if mode == encrypt_option:
+            message = file_reader(input_file)
+            #Generating a random key
+            key = key_generator(message)
+            ciphertext = xor_fuction(key, message)
+            #Encoding
+            byte_ciphertext = ciphertext.encode("utf-8")
+            ciphertext = base64.b64encode(byte_ciphertext)
 
-        return key, ciphertext
+            return_key, return_message = key, ciphertext
+        
+        #For Decryption, we will use user provided key
+        elif mode == decrypt_option:
+            key = user_key        
+            message = file_reader(input_file)
+            #Decoding the base64
+            try:
+                plain_message = base64.b64decode(message)
+                plain_message = plain_message.decode("utf-8")
+                if len(key) != len(plain_message):
+                    print("[-] Key length and message length doesn't match")
+                    exit()
+                else:
+                    plaintext = xor_fuction(user_key, plain_message)
+                    return_key, return_message = key, plaintext
+            except:
+                print("\nSomething went wron while decrypting the data....")
+                print("Make sure that provided file contain base64 encoded data..\n")
+        else:
+            print("[!] Please select correct mode")
     
-    #For Decryption, we will use user provided key
-    else:
-        key = user_key        
-        message = file_reader(input_file)
-        #Decoding the base64
-        try:
-            plain_message = base64.b64decode(message)
-            plain_message = plain_message.decode("utf-8")
-            if len(key) != len(plain_message):
-                print("[-] Key length and message length doesn't match")
-                exit()
-            else:
-                plaintext = xor_fuction(user_key, plain_message)
-                return key, plaintext
-        except:
-            print("Something went wron while decrypting the data....")
-            print("Make sure that provided file contain base64 encoded data..\n")
+    except Exception as error:
+        return f"[-] One-Time-Pad function crashed with Error\n{error}"
+    
+    return return_key, return_message
 
 #CLI Argument Parser
 def initializing():
@@ -104,9 +111,9 @@ def initializing():
     parser = argparse.ArgumentParser(description="One-Time Pad (OTP): Encrypt and Decrypt.")    
     
     #Passing the Key
-    parser.add_argument("-k", "--key", help="Provide the key for decryption", type=str, default=None)
+    parser.add_argument("-k", "--key", help="Provide the key for decryption", type=str)
     #Passing the input file
-    parser.add_argument("-i", "--input", help="Provide an Input file, ciphertext/plaintext", required=True, default=None, type=str)    
+    parser.add_argument("-i", "--input", help="Provide an Input file, ciphertext/plaintext", required=True, type=str)    
     
     #Option for either using encryption or decryption mode
     encrypt_decrypt_group = parser.add_mutually_exclusive_group(required=True)
@@ -134,8 +141,8 @@ if __name__ == "__main__":
         #Write the ciphertext
         file_writter(key, ciphertext, None)
         print(f"\n[+] ENCRYPTION KEY:{key}")
-        print(f"\n[+] Message Encrypted, Key and Cipher is stored in ciphertext_log.txt\n")
-        print(f"Encrypted message stored in 'cipher.txt'\n")
+        print(f"\n[+] Message Encrypted.....Key and the Ciphertext is stored in otp_log.txt\n")
+        print(f"[+] Encrypted message stored in 'ciphertext.txt'\n")
         # print(f"CIPHERTEXT:{ciphertext}")
             
     elif decrypt_option:
@@ -148,4 +155,3 @@ if __name__ == "__main__":
             key, plaintext = one_time_pad(user_key, decrypt_option, file_input)            
             print(f"[+] DECRYPTED TEXT: {plaintext}\n")
             file_writter(key, None, plaintext)
-
